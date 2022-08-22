67017e183f8d66c85dbbde6addabb5a94c45c605
Alina Vasilciuc <alina.vasilciuc@hotmail.com>
2022-08-22 15:30:24 +0100

    Single Page Applications Part 2
    introduced the angular route parameters.

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -7,16 +11,22 @@ import { Dish } from '../shared/dish';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.less']
 })
-export class DishdetailComponent implements OnInit {
-  // introduce a new variable in the dishdetail.component.ts file in the dishdetail class called dish and set it equal to the DISH constant above:
-  selectedDish =  Dish;
-  
-  constructor() { }
 
-  ngOnInit() {
+export class DishdetailComponent implements OnInit {
+
+  dish: Dish;
+
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
+
+  ngOnInit() {  // allows for /dishdetail/0 or /dishdetail/1, ...
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
 
-  @Input()
-  dish: Dish;
-  
+  goBack(): void {
+    this.location.back();
+  }
+
 }
