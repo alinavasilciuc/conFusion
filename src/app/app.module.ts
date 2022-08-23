fdd9dd764a9bef8e96343d6d97db8a662435820e
Alina Vasilciuc <alina.vasilciuc@hotmail.com>
2022-08-23 17:21:39 +0100

    Reactive Forms - part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import 'hammerjs';
 
@@ -56,8 +60,10 @@ import { LoginComponent } from './login/login.component'
     MatDialogModule,
     MatFormFieldModule, 
     MatInputModule,
-    MatCheckboxModule
-    
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
 
   ],
   providers: [
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.less']
 })
+
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup;  //form model that is going to host the Reactive form 
+  feedback: Feedback;       // variable Feedback, corresponding data model
+  contactType = ContactType;
+
+  // Inject the FormBuilder service into the constructer
+  constructor(private fb: FormBuilder) {
+    this.createForm(); // when class is built the form will be also created 
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
